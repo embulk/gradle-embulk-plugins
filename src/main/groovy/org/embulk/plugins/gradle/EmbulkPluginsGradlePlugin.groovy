f453e5323f4a5f9258649171df8299a55c7731b9
Dai MIKURUBE <dmikurube@treasure-data.com>
2017-09-13 13:34:56 +0900

    Add task class "MavenUploadEmbulkPluginJar" with default task "uploadEmbulkPluginJar"

--- src/main/groovy/org/embulk/plugins/gradle/EmbulkPluginsGradlePlugin.groovy
+++ src/main/groovy/org/embulk/plugins/gradle/EmbulkPluginsGradlePlugin.groovy
@@ -1,6 +1,7 @@
 package org.embulk.plugins.gradle
 
 import org.embulk.plugins.gradle.tasks.EmbulkPluginJar
+import org.embulk.plugins.gradle.tasks.MavenUploadEmbulkPluginJar
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -8,11 +9,19 @@ class EmbulkPluginsGradlePlugin implements Plugin<Project> {
     @Override
     void apply(final Project project) {
         configureEmbulkPluginJarTask(project)
+        configureUploadEmbulkPluginJarTask(project)
     }
 
     protected void configureEmbulkPluginJarTask(final Project project) {
         EmbulkPluginJar embulkPluginJarTask = project.tasks.create("embulkPluginJar", EmbulkPluginJar)
         embulkPluginJarTask.group = "org.embulk.gradle"
         embulkPluginJarTask.description = 'Create an Embulk plugin JAR file'
+    }
+
+    private void configureUploadEmbulkPluginJarTask(final Project project) {
+        final MavenUploadEmbulkPluginJar uploadEmbulkPluginJarTask =
+            project.tasks.create("uploadEmbulkPluginJar", MavenUploadEmbulkPluginJar)
+        uploadEmbulkPluginJarTask.group = "org.embulk.plugins.gradle"
+        uploadEmbulkPluginJarTask.description = 'Upload an Embulk plugin JAR file to a Maven repository'
     }
 }
